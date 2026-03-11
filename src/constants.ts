6a6d69ad79585faa815d04c8a6b5d611ddb3421d
lilhuy0405 <luuhuy040501.travail@gmail.com>
2026-03-12 00:20:41 +0700

    Update logo size and share links titles. Add demo gallery

--- src/constants.ts
+++ src/constants.ts
@@ -47,37 +47,37 @@ export const SHARE_LINKS: Social[] = [
   {
     name: "WhatsApp",
     href: "https://wa.me/?text=",
-    linkTitle: `Envia este post por WhatsApp`,
+    linkTitle: `Share this post on WhatsApp`,
     icon: IconWhatsapp,
   },
   {
     name: "Facebook",
     href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Comparte este post en Facebook`,
+    linkTitle: `Share this post on Facebook`,
     icon: IconFacebook,
   },
   {
     name: "X",
     href: "https://x.com/intent/post?url=",
-    linkTitle: `Comparte este post en X`,
+    linkTitle: `Share this post on X`,
     icon: IconBrandX,
   },
   {
     name: "Telegram",
     href: "https://t.me/share/url?url=",
-    linkTitle: `Comparte este post por Telegram`,
+    linkTitle: `Share this post on Telegram`,
     icon: IconTelegram,
   },
   {
     name: "Pinterest",
     href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Comparte este post en Pinterest`,
+    linkTitle: `Share this post on Pinterest`,
     icon: IconPinterest,
   },
   {
     name: "Mail",
     href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Envia este post por correo`,
+    linkTitle: `Share this post by email`,
     icon: IconMail,
   },
 ] as const;
---
+++ src/data/galleries/demo/index.md
@@ -0,0 +1,11 @@
+---
+title: Demo Galleries
+description: A demo galleries.
+pubDatetime: 2026-02-10T09:00:00Z
+tags:
+  - demo
+  - image
+draft: false
+---
+
+Demo images.
